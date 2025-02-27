f7d3090ce3f6545da7cae4b69c9c9ce8b417f54c
Janne Salokoski <salokoski.janne@gmail.com>
2025-02-27 13:07:10 +0200

    Add collections, groups and resources

--- server/app/models/models.py
+++ server/app/models/models.py
@@ -71,3 +71,91 @@ class PublicUser(BaseUser):
 
 class UserResponse(BaseResponse):
     users: list[PublicUser]
+
+
+## Collections ##
+
+
+class BaseCollection(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBCollection(BaseCollection):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    active: bool = Field()
+
+
+class CreateCollection(BaseCollection):
+    pass
+
+
+class UpdateCollection(BaseCollection):
+    pass
+
+
+class PublicCollection(BaseCollection):
+    pass
+
+
+class CollectionResponse(BaseResponse):
+    collections: list[PublicCollection]
+
+
+## Groups ##
+
+
+class BaseGroup(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBGroup(BaseGroup):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    collection_id: Optional[int] = Field(default=None, foreign_key="dbcollection.id")
+    active: bool = Field()
+
+
+class CreateGroup(BaseGroup):
+    pass
+
+
+class UpdateGroup(BaseGroup):
+    collection_id: int
+
+
+class PublicGroup(BaseGroup):
+    id: int
+    collection_id: int
+
+
+class GroupResponse(BaseResponse):
+    collections: list[PublicGroup]
+
+
+## Resources ##
+
+
+class BaseResource(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBResource(BaseResource):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    group_id: Optional[int] = Field(default=None, foreign_key="dbgroup.id")
+    active: bool = Field()
+
+
+class CreateResource(BaseResource):
+    pass
+
+
+class UpdateResource(BaseResource):
+    group_id: int
+
+
+class PublicResource(BaseResource):
+    id: int
+    group_id: int
+
+
+class ResourceResponse(BaseResponse):
+    collections: list[PublicResource]
