3c08e1880225b0caa7523566510ab0e20ebdb012
Janne Salokoski <salokoski.janne@gmail.com>
2025-02-27 11:55:17 +0200

    Update User -model, create an Org -model and update their endpoints
    Create a new Org model, which the updated User references to
    
    Also add an endpoint to add users

--- server/app/api/endpoints.py
+++ server/app/api/endpoints.py
@@ -1,22 +1,49 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import APIRouter, Query
 from sqlmodel import select
 
 from app.db.session import SessionDep
-from app.models.models import UserBase, User, PublicUser
+from app.models.models import CreateUser, DBUser, PublicUser, UserResponse
 
 router = APIRouter()
 
 
-@router.get("/users/", response_model=list[PublicUser])
+@router.get("/users", response_model=UserResponse)
 async def read_users(
     session: SessionDep,
+    org: Optional[int] = None,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[PublicUser]:
-    query = select(UserBase).offset(offset).limit(limit)
-    result = await session.execute(query)
-    users = result.scalars().all()
+) -> UserResponse:
+    query = select(DBUser).where(DBUser.active)
 
-    return users
+    if org:
+        query = query.where(DBUser.org_id == org)
+
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    users: list[PublicUser] = result.scalars().all()
+
+    more_available = len(users) > limit
+
+    response = UserResponse(status=True, more_available=more_available, users=users)
+
+    return response
+
+
+@router.post("/users", response_model=UserResponse)
+async def create_user(session: SessionDep, user: CreateUser):
+    db_user = DBUser(username=user.username, hash=user.hash)
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+
+    print(db_user)
+
+    public_user = PublicUser.from_orm(db_user)
+
+    print(public_user)
+
+    response = UserResponse(status=True, more_available=False, users=[public_user])
+    return response
--- server/app/models/models.py
+++ server/app/models/models.py
@@ -1,17 +1,65 @@
 # from sqlalchemy import Column, Integer, String
+from sqlmodel import Field, SQLModel
+from pydantic import BaseModel
+
 from typing import Optional
-from sqlmodel import SQLModel, Field
 
 
-class UserBase(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
+class BaseResponse(BaseModel):
+    status: bool
+    more_available: bool
 
 
-class User(UserBase):
+## Organizations ##
+
+
+class BaseOrg(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBOrg(BaseOrg, table=True):
+    id: int = Field(default=None, primary_key=True)
+    active: bool = Field(default=True)
+
+
+class PublicOrg(BaseOrg):
     id: int
     name: str
+    active: bool
 
 
-class PublicUser(UserBase):
-    name: str
+class OrgResponse(BaseResponse):
+    orgs: list[PublicOrg]
+
+
+## Users ##
+
+
+class BaseUser(SQLModel):
+    username: str = Field(default=None, index=True)
+
+
+class DBUser(BaseUser, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    hash: str = Field(default=None)
+    active: bool = Field(default=True)
+    org_id: Optional[int] = Field(default=None, foreign_key="dborg.id")
+
+
+class CreateUser(BaseUser):
+    hash: str
+
+
+class UpdateUser(BaseUser):
+    id: int
+    hash: str
+    organization_id: int | None
+
+
+class PublicUser(BaseUser):
+    id: int
+    active: bool
+
+
+class UserResponse(BaseResponse):
+    users: list[PublicUser]
