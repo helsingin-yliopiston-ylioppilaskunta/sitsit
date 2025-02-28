f1ee14507b0611515c29c9d03ae48a808b49a877
Janne Salokoski <salokoski.janne@gmail.com>
2025-02-28 14:30:06 +0200

    Revert "Use Response[type] on all endpoints"
    The idea of splitting code was good, but fastapi couldn't handle that
    :(
    
    This reverts commit e92d595a052477af4139ffcfa62a6c53a36dbf13.

---
+++ server/app/api/endpoints.py
@@ -0,0 +1,230 @@
+from typing import Annotated, Optional
+
+from fastapi import APIRouter, Query
+from sqlmodel import select
+
+from sqlalchemy.orm import selectinload
+
+from app.db.session import SessionDep
+from app.models.models import (
+    CreateUser,
+    DBUser,
+    PublicUser,
+    PublicUserWithOrg,
+    UserResponse,
+    CreateOrg,
+    DBOrg,
+    PublicOrg,
+    PublicOrgWithUsers,
+    OrgResponse,
+    CreateCollection,
+    DBCollection,
+    PublicCollection,
+    CollectionResponse,
+    CreateGroup,
+    DBGroup,
+    PublicGroup,
+    GroupResponse,
+    CreateResource,
+    DBResource,
+    PublicResource,
+    ResourceResponse,
+    Response,
+)
+
+router = APIRouter()
+
+
+@router.get("/users", response_model=Response[PublicUserWithOrg])
+async def read_users(
+    session: SessionDep,
+    org: Optional[int] = None,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> Response[PublicUserWithOrg]:
+    query = select(DBUser).where(DBUser.active).options(selectinload(DBUser.org))
+
+    if org:
+        query = query.where(DBUser.org_id == org)
+
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    users: list[PublicUserWithOrg] = result.scalars().all()
+
+    more_available = len(users) > limit
+
+    response = Response(status=True, more_available=more_available, items=users)
+
+    return response
+    # return users
+
+
+@router.post("/users", response_model=UserResponse)
+async def create_user(session: SessionDep, user: CreateUser):
+    db_user = DBUser(username=user.username, hash=user.hash, org_id=user.org_id)
+    session.add(db_user)
+    await session.commit()
+    await session.refresh(db_user)
+
+    print(db_user)
+
+    public_user = PublicUser.from_orm(db_user)
+
+    print(public_user)
+
+    response = UserResponse(status=True, more_available=False, users=[public_user])
+    return response
+
+
+# Organizations
+
+
+@router.get("/orgs", response_model=OrgResponse)
+async def read_orgs(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> OrgResponse:
+    query = select(DBOrg).where(DBOrg.active).options(selectinload(DBOrg.users))
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    orgs: list[PublicOrgWithUsers] = result.scalars().all()
+
+    more_available = len(orgs) > limit
+
+    response = OrgResponse(
+        status=True, more_available=more_available, organizations=orgs
+    )
+
+    return response
+
+
+@router.post("/orgs", response_model=OrgResponse)
+async def create_org(session: SessionDep, org: CreateOrg):
+    db_org = DBOrg(name=org.name)
+    session.add(db_org)
+    await session.commit()
+    await session.refresh(db_org)
+
+    public_org = PublicOrg.from_orm(db_org)
+
+    response = OrgResponse(
+        status=True, more_available=False, organizations=[public_org]
+    )
+    return response
+
+
+# Collections
+
+
+@router.get("/collections", response_model=CollectionResponse)
+async def read_collections(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> CollectionResponse:
+    query = select(DBCollection).where(DBCollection.active)
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    collections: list[PublicCollection] = result.scalars().all()
+
+    print(collections)
+
+    more_available = len(collections) > limit
+
+    response = CollectionResponse(
+        status=True, more_available=more_available, collections=collections
+    )
+
+    return response
+
+
+@router.post("/collections", response_model=CollectionResponse)
+async def create_collection(session: SessionDep, org: CreateCollection):
+    db_collection = DBCollection(name=org.name)
+    session.add(db_collection)
+    await session.commit()
+    await session.refresh(db_collection)
+
+    public_collection = PublicCollection.from_orm(db_collection)
+
+    response = CollectionResponse(
+        status=True, more_available=False, collections=[public_collection]
+    )
+    return response
+
+
+## Groups ##
+
+
+@router.get("/groups", response_model=GroupResponse)
+async def read_groups(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> GroupResponse:
+    query = select(DBGroup).where(DBGroup.active)
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    groups: list[PublicGroup] = result.scalars().all()
+
+    print(groups)
+
+    more_available = len(groups) > limit
+
+    response = GroupResponse(status=True, more_available=more_available, groups=groups)
+
+    return response
+
+
+@router.post("/groups", response_model=GroupResponse)
+async def create_group(session: SessionDep, group: CreateGroup):
+    db_group = DBGroup(name=group.name, collection_id=group.collection_id)
+    session.add(db_group)
+    await session.commit()
+    await session.refresh(db_group)
+
+    public_group = PublicGroup.from_orm(db_group)
+
+    response = GroupResponse(status=True, more_available=False, groups=[public_group])
+    return response
+
+
+## Resources ##
+
+
+@router.get("/resources", response_model=ResourceResponse)
+async def read_resources(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> ResourceResponse:
+    query = select(DBResource).where(DBResource.active)
+    query = query.offset(offset).limit(limit + 1)
+    result = await session.execute(query)
+    resources: list[PublicResource] = result.scalars().all()
+
+    print(resources)
+
+    more_available = len(resources) > limit
+
+    response = ResourceResponse(
+        status=True, more_available=more_available, resources=resources
+    )
+
+    return response
+
+
+@router.post("/resources", response_model=ResourceResponse)
+async def create_resource(session: SessionDep, org: CreateResource):
+    db_resource = DBResource(name=org.name, group_id=org.group_id)
+    session.add(db_resource)
+    await session.commit()
+    await session.refresh(db_resource)
+
+    public_resource = PublicResource.from_orm(db_resource)
+
+    response = ResourceResponse(
+        status=True, more_available=False, resources=[public_resource]
+    )
+    return response
---
+++ server/app/models/models.py
@@ -0,0 +1,373 @@
+# from sqlalchemy import Column, Integer, String
+from sqlmodel import Field, SQLModel, Relationship
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+
+class Response[T](BaseModel):
+    status: bool
+    more_available: bool
+    items: list[T]
+
+
+class BaseResponse(BaseModel):
+    status: bool
+    more_available: bool
+
+
+## Organizations ##
+
+
+class BaseOrg(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBOrg(BaseOrg, table=True):
+    id: int = Field(default=None, primary_key=True)
+    active: bool = Field(default=True)
+    users: list["DBUser"] = Relationship(back_populates="org")
+
+
+class CreateOrg(BaseOrg):
+    pass
+
+
+class UpdateOrg(BaseOrg):
+    pass
+
+
+class PublicOrg(BaseOrg):
+    id: int
+    name: str
+
+
+class PublicOrgWithUsers(BaseOrg):
+    id: int
+    name: str
+    users: list["DBUser"]
+
+
+class OrgResponse(BaseResponse):
+    organizations: list[PublicOrg | PublicOrgWithUsers]
+
+
+## Users ##
+
+
+class BaseUser(SQLModel):
+    username: str = Field(default=None, index=True)
+
+
+class DBUser(BaseUser, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    hash: str = Field(default=None)
+    active: bool = Field(default=True)
+    org_id: Optional[int] = Field(default=None, foreign_key="dborg.id")
+    org: DBOrg = Relationship(back_populates="users")
+
+
+class CreateUser(BaseUser):
+    hash: str
+    org_id: int
+
+
+class UpdateUser(BaseUser):
+    id: int
+    hash: str
+    organization_id: int | None
+
+
+class PublicUser(BaseUser):
+    id: int
+    active: bool
+    org_id: Optional[int]
+
+
+class PublicUserWithOrg(BaseUser):
+    id: int
+    org: DBOrg | None
+
+
+class UserResponse(BaseResponse):
+    users: list[PublicUser | PublicUserWithOrg]
+
+
+## Collections ##
+
+
+class BaseCollection(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBCollection(BaseCollection, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    active: bool = Field(default=True)
+
+
+class CreateCollection(BaseCollection):
+    pass
+
+
+class UpdateCollection(BaseCollection):
+    pass
+
+
+class PublicCollection(BaseCollection):
+    id: int
+
+
+class CollectionResponse(BaseResponse):
+    collections: list[PublicCollection]
+
+
+## Groups ##
+
+
+class BaseGroup(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBGroup(BaseGroup, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    collection_id: Optional[int] = Field(default=None, foreign_key="dbcollection.id")
+    active: bool = Field(default=True)
+
+
+class CreateGroup(BaseGroup):
+    collection_id: int
+
+
+class UpdateGroup(BaseGroup):
+    collection_id: int
+
+
+class PublicGroup(BaseGroup):
+    id: int
+    collection_id: int
+
+
+class GroupResponse(BaseResponse):
+    groups: list[PublicGroup]
+
+
+## ResourceTypes ##
+
+
+class BaseResourceType(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBResourceType(BaseResourceType, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    active: bool = Field(default=True)
+
+
+class CreateResourceType(BaseResourceType):
+    pass
+
+
+class UpdateResourceType(BaseResourceType):
+    pass
+
+
+class PublicResourceType(BaseResourceType):
+    id: int
+
+
+class ResourceTypeResponse(BaseResponse):
+    resourceTypes: list[PublicResourceType]
+
+
+## Resources ##
+
+
+class BaseResource(SQLModel):
+    name: str = Field(default=None, index=True)
+
+
+class DBResource(BaseResource, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    group_id: Optional[int] = Field(default=None, foreign_key="dbgroup.id")
+    resource_type_id: Optional[int] = Field(
+        default=None, foreign_key="dbresourcetype.id"
+    )
+    active: bool = Field(default=True)
+
+
+class CreateResource(BaseResource):
+    group_id: int
+    resource_type_id: int
+
+
+class UpdateResource(BaseResource):
+    group_id: int
+    resource_type_id: int
+
+
+class PublicResource(BaseResource):
+    id: int
+    group_id: int
+    resource_type_id: int
+
+
+class ResourceResponse(BaseResponse):
+    resources: list[PublicResource]
+
+
+## Reservations ##
+
+
+class BaseReservation(SQLModel):
+    name: str = Field(default=None)
+
+
+class DBReservation(BaseReservation, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: Optional[int] = Field(default=None, foreign_key="dbuser.id")
+    org_id: Optional[int] = Field(default=None, foreign_key="dborg.id")
+    description: Optional[str] = Field()
+    active: bool = Field(default=True)
+
+
+class CreateReservation(BaseReservation):
+    user_id: int
+    org_id: int
+    contact_info: Optional[str]
+    description: Optional[str]
+
+
+class UpdateReservation(BaseReservation):
+    user_id: int
+    org_id: int
+    contact_info: Optional[str]
+    description: Optional[str]
+
+
+class PublicReservation(BaseReservation):
+    id: int
+    user_id: int
+    org_id: int
+    contact_info: Optional[str]
+    description: Optional[str]
+
+
+class ReservationResponse(BaseResponse):
+    reservations: list[PublicReservation]
+
+
+## ReservationInfo ##
+
+
+class BaseReservationInfo(SQLModel):
+    reservation_id: int = Field(
+        default=None, index=True, foreign_key="dbreservation.id"
+    )
+
+
+class DBReservationInfo(BaseReservationInfo, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    reserver: Optional[str] = Field(index=True)
+    contact: Optional[str] = Field()
+    participants: Optional[int] = Field(default=0)
+    description: Optional[str] = Field()
+    notes: Optional[str] = Field()
+    active: bool = Field(default=True)
+
+
+class CreateReservationInfo(BaseReservationInfo):
+    reservation_id: int
+    reserver: Optional[str]
+    contact: Optional[str]
+    participants: Optional[str]
+    description: Optional[str]
+    notes: Optional[str]
+
+
+class UpdateReservationInfo(BaseReservationInfo):
+    reserver: Optional[str]
+    contact: Optional[str]
+    participants: Optional[str]
+    description: Optional[str]
+    notes: Optional[str]
+
+
+class PublicReservationInfo(BaseReservationInfo):
+    id: int
+
+
+class PrivateReservationInfo(BaseReservationInfo):
+    id: int
+    reserver: Optional[str]
+    contact: Optional[str]
+    participants: Optional[str]
+    description: Optional[str]
+    notes: Optional[str]
+
+
+class ReservationInfoResponse(BaseResponse):
+    reservations: list[PublicReservationInfo | PrivateReservationInfo]
+
+
+## ReservationTime ##
+
+
+class BaseReservationTime(SQLModel):
+    reservation_id: int = Field(
+        default=None, index=True, foreign_key="dbreservation.id"
+    )
+
+
+class DBReservationTime(BaseReservationTime):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    active: bool = Field(default=True)
+
+
+class CreateReservationTime(BaseReservationTime):
+    timestamp: datetime
+
+
+class ModifyReservationTime(BaseReservationTime):
+    timestamp: datetime
+
+
+class PublicReservationTime(BaseReservationTime):
+    id: int
+    timestamp: int
+
+
+class ReservationTimeResponse(BaseResponse):
+    times: list[PublicReservationTime]
+
+
+## ReservationPlace ##
+
+
+class BaseReservationResource(SQLModel):
+    reservation_id: int = Field(
+        default=None, index=True, foreign_key="dbreservation.id"
+    )
+
+
+class DBReservationResource(BaseReservationResource):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    resource_id: int = Field(default=None, index=True, foreign_key="dbresource.id")
+    active: bool = Field(default=True)
+
+
+class CreateReservationResource(BaseReservationResource):
+    resource_id: int
+
+
+class ModifyReservationResource(BaseReservationResource):
+    resource_id: int
+
+
+class PublicReservationResource(BaseReservationResource):
+    id: int
+    resource_id: int
+
+
+class ReservationResourceResponse(BaseResponse):
+    resources: list[PublicReservationResource]
