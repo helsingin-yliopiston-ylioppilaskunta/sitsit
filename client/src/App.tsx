d03f26ec0f06fc2a83fd2f84a0b01a7de483f991
Janne Salokoski <salokoski.janne@gmail.com>
2025-03-04 14:06:25 +0200

    Start work on UI developement
    Add a node project with vite, react and typescript!

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+        <p>Moi</p>
+            <input type="button" value="Hei!"></input>
+    </div>
+  )
+}
+
+export default App
