7800bd7d0bfd9280065f668af99e20822db35ef6
Janne Salokoski <salokoski.janne@gmail.com>
2025-03-04 15:49:06 +0200

    Add a proof of concept user component
    Test how could we get and set data for a user

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
+import User from "./components/User";
+
 function App() {
-  return (
-    <div className="App">
-        <p>Moi</p>
-            <input type="button" value="Hei!"></input>
-    </div>
-  )
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const response = await fetch("http://localhost:8000/users/1");
+            const data = await response.json();
+
+            setUserData(data);
+            setLoading(false);
+        }
+
+        fetchUser();
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    else {
+        return (
+            <div className="App">
+                <p>Moi</p>
+                <input type="button" value="Hei!"></input>
+                <User userData={userData.items[0]} />
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ client/src/components/User.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './User.css'
+
+function User(props) {
+    console.log(props.userData);
+    const [username, setUsername] = useState(props.userData.username);
+
+    function saveUser() {
+        const user = {
+            username: username,
+            hash: "xxx",
+            org_id: 1
+        };
+
+        async function uploadUser() {
+            const response = await fetch(`http://localhost:8000/users/${props.userData.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+            const data = await response.json();
+
+            console.log(data)
+        }
+
+        uploadUser();
+    }
+
+
+    return (
+        <div className="User">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                saveUser();
+            }}>
+                <div className="row">
+                    <label>Id</label>
+                    <input type="number" disabled="disabled" value={props.userData.id} />
+                </div>
+                <div>
+                    <label>Username</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <input type="submit" value="Tallenna" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default User
